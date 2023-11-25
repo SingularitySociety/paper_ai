921a0273d7d96f238d84b68faffde38382f2e5c7
isamu <isamu@to-kyo.to>
2023-11-25 15:56:20 +0900

    arXiv-api

---
+++ functions/tests/arxiv.ts
@@ -0,0 +1,26 @@
+import { search } from 'arxiv-api';
+import * as fs from 'fs';
+ 
+const main = async () => {
+  try {
+    const papers = await search({
+      searchQueryParams: [
+        {
+          include: [{name: 'LLM'}],
+        },
+      ],
+      sortBy: "lastUpdatedDate",
+      sortOrder: "descending",
+      start: 0,
+      maxResults: 100,
+    });
+    
+    console.log(papers)
+    const json = JSON.stringify(papers, null, 2);
+    fs.writeFileSync('tests/export.json', json);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+main();
